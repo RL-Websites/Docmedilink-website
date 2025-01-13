3cc7db24374e87989bdc5f7b12e7dbd1938e105c
Muhammad Hannan <mdhannan.info@gmail.com>
2025-01-13 16:19:38 +0600

    Update

--- resources/views/layouts/frontend_main.blade.php
+++ resources/views/layouts/frontend_main.blade.php
@@ -109,7 +109,7 @@
                     <li><a href="{{ url('/') }}#about-us" class="menu-link">About us</a></li>
                     <li><a href="{{ url('/') }}#contact-us" class="menu-link active">Contact us</a></li>
                     <li class="sm-block w-[100px]">
-                        <a target="_blank" href="https://app.docmedilink.com/login"
+                        <a target="_blank" href="{{ url('login') }}"
                             class="dml-btn dml-btn__primary">Login</a>
                     </li>
                 </ul>
