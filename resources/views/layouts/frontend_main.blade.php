aed8039e595e0ad3cf5cf2a5eb9d6d277c0a8b45
obaedulislam <obaed.cse@gmail.com>
2025-01-15 10:54:28 +0600

    Extra button removed from home page

--- resources/views/layouts/frontend_main.blade.php
+++ resources/views/layouts/frontend_main.blade.php
@@ -121,11 +121,6 @@
                             class="dml-btn dml-btn__primary ">Login</a>
                     </li>
                 </ul>
-
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded test-button">
-                    Button
-                  </button>
-
             </nav>
 
             <div class="sm-hidden">
