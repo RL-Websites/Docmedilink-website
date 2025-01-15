481be113c87e069cfa7d443818a8e815f65c81fd
obaedulislam <obaed.cse@gmail.com>
2025-01-15 09:12:56 +0600

    Footer animation changed & Read More button text to Learn More

--- resources/views/layouts/frontend_main.blade.php
+++ resources/views/layouts/frontend_main.blade.php
@@ -155,19 +155,16 @@
 							class="md:mx-[unset] mx-auto"
 							src="assets/img/logo.svg"
 							alt="DML Logo"
-							data-aos="fade-up"
-							data-aos-duration="500"
+						
 						/>
 						<p
 							class="lg:text-xl md:text-lg text-base lg:mt-5 mt-2"
-							data-aos="fade-up"
-							data-aos-duration="700"
+				
 							>&copy; 2025 Docmedilink. Inc.</p
 						>
 						<div
 							class="flex items-center md:justify-start justify-center lg:gap-10 gap-5 lg:mt-16 mt-7"
-							data-aos="fade-up"
-							data-aos-duration="700"
+						
 						>
 							<img
 								class="w-[100px]"
@@ -185,45 +182,26 @@
 						class="col-span-2 grid md:grid-cols-3 md:text-start text-center md:mt-0 mt-8"
 					>
 						<div>
-							<h4 class="font-bold" data-aos="fade-up" data-aos-duration="500"
-								>Quick Links</h4
-							>
-                            <ul class="footer__menus flex flex-col md:gap-7 gap-4 mt-5 aos-init"data-aos="fade-up"
-                            data-aos-duration="700">
-                            <li><a class="active" href="/">Home</a></li>
-                            <li><a href="{{ url('our-services') }}">Services</a></li>
-                            <li><a href="#clinicians">Clinicians</a></li>
-                            <li><a href="/#about-us">About us</a></li>
-                            <li><a href="/#contact-us">Contact us</a></li>
-                        </ul>
+							<h4 class="font-bold" >Quick Links</h4>
+                            <ul class="footer__menus flex flex-col md:gap-7 gap-4 mt-5 aos-init">
+                                <li><a class="active" href="/">Home</a></li>
+                                <li><a href="{{ url('our-services') }}">Services</a></li>
+                                <li><a href="#clinicians">Clinicians</a></li>
+                                <li><a href="/#about-us">About us</a></li>
+                                <li><a href="/#contact-us">Contact us</a></li>
+                            </ul>
 						</div>
 						<div class="md:mt-0 mt-12">
-							<h4 class="font-bold" data-aos="fade-up" data-aos-duration="500"
-								>Social Links</h4
-							>
-							<ul
-								class="flex flex-col lg:gap-7 gap-4 mt-5"
-								data-aos="fade-up"
-								data-aos-duration="700"
-							>
+							<h4 class="font-bold">Social Links</h4>
+							<ul class="flex flex-col lg:gap-7 gap-4 mt-5">
 								<li><a href="">Linkedin</a></li>
 								<li><a href="#">Facebook</a></li>
 							</ul>
 						</div>
 						<div class="md:mt-0 mt-12">
-							<h4 class="font-bold" data-aos="fade-up" data-aos-duration="500"
-								>Contacts</h4
-							>
-							<ul
-								class="flex flex-col md:gap-7 gap-4 mt-5"
-								data-aos="fade-up"
-								data-aos-duration="700"
-							>
-								<li
-									><a href="mailto:info@docmedilink.com"
-										>info@docmedilink.com</a
-									></li
-								>
+							<h4 class="font-bold" >Contacts</h4>
+							<ul class="flex flex-col md:gap-7 gap-4 mt-5">
+								<li><a href="mailto:info@docmedilink.com">info@docmedilink.com</a></li>
 								<li><a href="tel:(972) 740-4622">(972) 740-4622</a></li>
 							</ul>
 						</div>
