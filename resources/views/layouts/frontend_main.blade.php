c0d7e8c5360209c14117b4ce2ae4708967027ead
obaedulislam <obaed.cse@gmail.com>
2025-01-15 09:38:41 +0600

    all changes done

--- resources/views/layouts/frontend_main.blade.php
+++ resources/views/layouts/frontend_main.blade.php
@@ -97,7 +97,7 @@
     <header class="header">
         <div class="container mx-auto flex justify-between items-center">
             <a href="{{ url('/') }}">
-                <img class="md:w-auto w-[170px]" src="assets/img/logo.svg" alt="Docmedilink Logo" />
+                <img class="md:w-auto  max-w-[200px]" src="assets/img/logo.svg" alt="Docmedilink Logo" />
             </a>
             <nav class="header__menus">
                 <ul class="flex gap-7">
@@ -138,69 +138,60 @@
     @yield('content')
 
     <!--====||  Footer Section Start ||====-->
-		<footer class="footer">
-			<div class="container mx-auto">
-				<div class="footer__content grid md:grid-cols-3 grid-cols-1">
-					<div class="col-span-1 md:text-start text-center">
-						<img
-							class="md:mx-[unset] mx-auto"
-							src="assets/img/logo.svg"
-							alt="DML Logo"
-						
-						/>
-						<p
-							class="lg:text-xl md:text-lg text-base lg:mt-5 mt-2"
-				
-							>&copy; 2025 Docmedilink. Inc.</p
-						>
-						<div
-							class="flex items-center md:justify-start justify-center lg:gap-10 gap-5 lg:mt-16 mt-7"
-						
-						>
-							<img
-								class="w-[100px]"
-								src="assets/img/hippa .svg"
-								alt="Hippa Logo"
-							/>
-							<img
-								class="w-[100px]"
-								src="assets/img/surescripts.svg"
-								alt="Surescript Logo"
-							/>
-						</div>
-					</div>
-					<div
-						class="col-span-2 grid md:grid-cols-3 md:text-start text-center md:mt-0 mt-8"
-					>
-						<div>
-							<h4 class="font-bold" >Quick Links</h4>
-                            <ul class="footer__menus flex flex-col md:gap-7 gap-4 mt-5 aos-init">
-                                <li><a class="active" href="/">Home</a></li>
-                                <li><a href="{{ url('our-services') }}">Services</a></li>
-                                <li><a href="#clinicians">Clinicians</a></li>
-                                <li><a href="/#about-us">About us</a></li>
-                                <li><a href="/#contact-us">Contact us</a></li>
-                            </ul>
-						</div>
-						<div class="md:mt-0 mt-12">
-							<h4 class="font-bold">Social Links</h4>
-							<ul class="flex flex-col lg:gap-7 gap-4 mt-5">
-								<li><a href="">Linkedin</a></li>
-								<li><a href="#">Facebook</a></li>
-							</ul>
-						</div>
-						<div class="md:mt-0 mt-12">
-							<h4 class="font-bold" >Contacts</h4>
-							<ul class="flex flex-col md:gap-7 gap-4 mt-5">
-								<li><a href="mailto:info@docmedilink.com">info@docmedilink.com</a></li>
-								<li><a href="tel:(972) 740-4622">(972) 740-4622</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-		<!--====||  footer Section End ||====-->
+    <footer class="footer">
+        <div class="container mx-auto">
+            <div class="footer__content grid md:grid-cols-3 grid-cols-1">
+                <div class="col-span-1 md:text-start text-center">
+                    <img class="md:mx-[unset] mx-auto max-w-[200px]" src="assets/img/logo.svg" alt="DML Logo"  />
+                    <p class="lg:text-xl md:text-lg text-base lg:mt-5 mt-2" >
+                        &copy; 2025 Docmedilink. Inc.
+                    </p>
+                    <div class="flex items-center md:justify-start justify-center lg:gap-10 gap-5 lg:mt-16 mt-7">
+                        <img class="w-[100px]" src="assets/img/hippa .svg" alt="Hippa Logo" />
+                        <img class="w-[100px]" src="assets/img/surescripts.svg" alt="Surescript Logo" />
+                    </div>
+                </div>
+                <div class="col-span-2 grid md:grid-cols-3 md:text-start text-center md:mt-0 mt-8">
+                    <div>
+                        <h4 class="font-bold" >Quick Links</h4>
+                        <ul class="footer__menus flex flex-col md:gap-7 gap-4 mt-5 aos-init">
+                            <li><a href="{{ request()->path() == '/' ? url('/#home') : '/' }}"
+                                    class="menu-link home {{ url()->current() === url('/') ? 'active' : '' }}">Home</a>
+                            </li>
+
+                            <li><a href="{{ request()->path() == '/' ? '#our-services' : url('/#our-services') }}"
+                                    class="menu-link our-services {{ request()->path() === 'our-services' ? 'active' : '' }}">Services</a>
+                            </li>
+
+                            <li><a href="{{ request()->path() == '/' ? '#clinicians' : url('/#clinicians') }}"
+                                    class="menu-link clinicians {{ request()->path() === 'clinicians' ? 'active' : '' }}">Clinicians</a>
+                            </li>
+                            <li><a href="{{ request()->path() == '/' ? '#about-us' : url('/#about-us') }}" class="menu-link about-us">About us</a></li>
+                            <li><a href="{{ request()->path() == '/' ? '#contact-us' : url('/#contact-us') }}" class="menu-link contact-us">Contact us</a></li>
+
+                        </ul>
+
+
+                    </div>
+                    <div class="md:mt-0 mt-12">
+                        <h4 class="font-bold" >Social Links</h4>
+                        <ul class="flex flex-col lg:gap-7 gap-4 mt-5" >
+                            <li><a href="">Linkedin</a></li>
+                            <li><a href="#">Facebook</a></li>
+                        </ul>
+                    </div>
+                    <div class="md:mt-0 mt-12" >
+                        <h4 class="font-bold" >Contacts</h4>
+                        <ul class="flex flex-col md:gap-7 gap-4 mt-5" >
+                            <li><a href="mailto:info@docmedilink.com">info@docmedilink.com</a></li>
+                            <li><a href="tel:(972) 740-4622">(972) 740-4622</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--====||  footer Section End ||====-->
 
     <!-- jQuery Script -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
